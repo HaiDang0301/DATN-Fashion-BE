9522b90daf84d98203460c5bd6e78df9709c4226
Hai Dang <dinhhaidang1003@gmail.com>
2023-09-12 20:18:04 +0700

    CRUD Collections

--- src/server.js
+++ src/server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const fileUpload = require("express-fileupload");
 const db = require("./config/db");
 const router = require("./routes/index");
 const app = express();
@@ -8,11 +7,6 @@ dotenv.config();
 const port = process.env.http;
 db.connect();
 app.use(express.json());
-app.use(
-  fileUpload({
-    useTempFiles: true,
-  })
-);
 router(app);
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
