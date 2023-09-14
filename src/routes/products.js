6f750c4acfb8d774a9e6f4ae8151770577d34452
Hai Dang <dinhhaidang1003@gmail.com>
2023-09-14 09:14:01 +0700

    add route feature/Admin-Products

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const products = require("../app/Controllers/ProductsController");
 const router = express.Router();
-router.get("/products/:category?", products.show);
+router.get("/products/:collections", products.index);
+router.get("/products/:slug/details", products.show);
 router.post("/products", products.store);
 router.put("/products/:id", products.update);
 router.delete("/products/:id", products.destroy);
