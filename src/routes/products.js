19693e8e22ab472747581c7c20d658e8acbff38d
Hai Dang <dinhhaidang1003@gmail.com>
2023-09-16 12:47:57 +0700

    CRUD Blogs

---
+++ src/routes/products.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const products = require("../app/Controllers/ProductsController");
+const router = express.Router();
+router.get("/products/:collections?", products.index);
+router.get("/products/:slug/details", products.show);
+router.get("/products/:id", products.edit);
+router.post("/products", products.store);
+router.put("/products/:id", products.update);
+router.delete("/products/:id", products.destroy);
+module.exports = router;
--- src/server.js
+++ src/server.js
@@ -1,18 +1,18 @@
 const express = require("express");
-const fileUpload = require("express-fileupload");
 const db = require("./config/db");
+const fileUpload = require("express-fileupload");
 const router = require("./routes/index");
 const app = express();
 const dotenv = require("dotenv");
 dotenv.config();
 const port = process.env.http;
 db.connect();
-app.use(express.json());
 app.use(
   fileUpload({
     useTempFiles: true,
   })
 );
+app.use(express.json());
 router(app);
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
