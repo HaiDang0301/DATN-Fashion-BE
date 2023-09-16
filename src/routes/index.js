86278d40e9fbdf937603a353a7009815a7624fe0
Hai Dang <dinhhaidang1003@gmail.com>
2023-09-16 12:46:11 +0700

    add route api feature/Admin-Blogs

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,7 @@
 const CollectionRouter = require("./collections");
+const BlogRouter = require("./blogs");
 function route(app) {
   app.use("/", CollectionRouter);
+  app.use("/", BlogRouter);
 }
 module.exports = route;
