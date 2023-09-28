7e59bb4c59371c183f858cfbecbfbb3792703a30
Hai Dang <dinhhaidang1003@gmail.com>
2023-09-28 18:23:02 +0700

    feature forget/reset password

--- src/app/Controllers/AuthsController.js
+++ src/app/Controllers/AuthsController.js
@@ -1,6 +1,7 @@
 const Accounts = require("../Models/AuthsModel");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const nodemailer = require("nodemailer");
 const dotenv = require("dotenv");
 dotenv.config();
 process.env.TOKEN_SECRET;
@@ -18,15 +19,35 @@ class AccountsController {
       if (findEmail) {
         res.status(409).json("The account has been registered");
       } else {
-        const salt = await bcrypt.genSalt(10);
-        const hashedPw = await bcrypt.hash(req.body.password, salt);
-        const account = new Accounts({
-          full_name: req.body.full_name,
-          email: req.body.email,
-          password: hashedPw,
+        const transporter = nodemailer.createTransport({
+          service: "gmail",
+          auth: {
+            user: process.env.Email_User,
+            pass: process.env.Email_Password,
+          },
         });
-        await account.save();
-        res.status(200).json("Successful account registration");
+        var mailOptions = {
+          from: process.env.Email_User,
+          to: req.body.email,
+          subject: "Notice: Registration Of Fashion Shop Account",
+          html: "<p>You have successfully registered the account</p>",
+          text: "click",
+        };
+        transporter.sendMail(mailOptions, async (error, info) => {
+          if (error) {
+            res.status(401).json("Can't find email");
+          } else {
+            const salt = await bcrypt.genSalt(10);
+            const hashedPw = await bcrypt.hash(req.body.password, salt);
+            const account = new Accounts({
+              full_name: req.body.full_name,
+              email: req.body.email,
+              password: hashedPw,
+            });
+            await account.save();
+            res.status(200).json("Successful account registration");
+          }
+        });
       }
     } catch (error) {
       res.status(500).json("Connect Server False");
@@ -61,6 +82,66 @@ class AccountsController {
     } catch (error) {
       res.status(500).send("Không thể kết nối đến server");
     }
+  }
+  async forget(req, res, next) {
+    try {
+      const email = req.body.email;
+      const user = await Accounts.findOne({ email: email });
+      if (!user) {
+        res.status(401).json("Can't not find email");
+      } else {
+        const token = jwt.sign({ id: user._id }, process.env.TOKEN_SECRET, {
+          expiresIn: "300s",
+        });
+        const transporter = nodemailer.createTransport({
+          service: "gmail",
+          auth: {
+            user: process.env.Email_User,
+            pass: process.env.Email_Password,
+          },
+        });
+        var mailOptions = {
+          from: process.env.Email_User,
+          to: req.body.email,
+          subject: "Notice: Reset Password",
+          html: `<p>You have just requested to reset the password. Please <a href ="${process.env.http_FE}/reset-password/${token}">click here</a></p>`,
+        };
+        transporter.sendMail(mailOptions, async (error, info) => {
+          if (error) {
+            res.status(401).json("Can't find email");
+          } else {
+            res
+              .status(200)
+              .json({ token, message: "Create a successful reset password" });
+          }
+        });
+      }
+    } catch (error) {
+      res.status(500).json("Connect Server False");
+    }
+  }
+  async reset(req, res, next) {
+    const token = req.params.token;
+    const password = req.body.password;
+    jwt.verify(token, process.env.TOKEN_SECRET, async (err, user) => {
+      if (err) {
+        res.status(401).json("Wrong Token");
+      } else {
+        const salt = await bcrypt.genSalt(10);
+        bcrypt
+          .hash(password, salt)
+          .then(async (hash) => {
+            const reset = await Accounts.findOneAndUpdate(
+              { _id: user.id },
+              { password: hash }
+            );
+            res.status(200).json("Reset Password Success");
+          })
+          .catch((err) => {
+            res.json(err);
+          });
+      }
+    });
   }
 }
 module.exports = new AccountsController();
--- src/routes/auths.js
+++ src/routes/auths.js
@@ -4,4 +4,6 @@ const Middleware = require("../middleware/Middleware");
 const router = express.Router();
 router.post("/api/auth/register", auths.register);
 router.post("/api/auth/login", auths.login);
+router.post("/api/auth/forget-password", auths.forget);
+router.post("/api/auth/reset-password/:token", auths.reset);
 module.exports = router;
